ff25f78cd3341c88176c01fa1973dc8487544f86
Michael Oberwasserlechner <michaelowl@gmail.com>
2015-12-31 17:44:49 +0100

    Add "subkey" parameter to forward and reverse request

--- src/main/java/com/byteowls/jopencage/model/JOpenCageRequest.java
+++ src/main/java/com/byteowls/jopencage/model/JOpenCageRequest.java
@@ -6,9 +6,27 @@ import java.util.Map;
 
 public abstract class JOpenCageRequest {
   
+  private String subkey;
+  
   public Map<String,String> getParameter() {
     Map<String, String> parameter = new HashMap<>();
+    if (subkey != null) {
+      parameter.put("subkey", subkey);
+    }
     return parameter;
+  }
+  
+  public String getSubkey() {
+    return subkey;
+  }
+
+  /**
+   * A unique id of your choosing (can contain only A-Za-z0-9 and with a maximum length of 20 characters). 
+   * The subkey is ignored by the geocoder but can be used for reporting. Not currently in use, but coming soon.
+   * @param subkey
+   */
+  public void setSubkey(String subkey) {
+    this.subkey = subkey;
   }
 
 }
