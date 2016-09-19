f0370c04ab8112ae74db74c2c946a22346a8dd92
Michael Oberwasserlechner <michaelowl@gmail.com>
2016-09-19 22:29:44 +0200

    explicitly set the timezone

--- src/test/java/com/byteowls/jopencage/ModelTests.java
+++ src/test/java/com/byteowls/jopencage/ModelTests.java
@@ -1,16 +1,16 @@
 package com.byteowls.jopencage;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.junit.Assert;
-import org.junit.Test;
-
 import com.byteowls.jopencage.model.JOpenCageResponse;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 
 public class ModelTests {
@@ -29,10 +29,9 @@ public class ModelTests {
     Assert.assertNotNull(jOpenCageResponse);
     
     Date createdAt = jOpenCageResponse.getTimestamp().getCreatedAt();
-    System.out.println("Raw date: " + createdAt);
     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    dateFormat.setTimeZone(TimeZone.getTimeZone("CET"));
     String formattedDate = dateFormat.format(createdAt);
-    System.out.println("Formatted date: " + formattedDate);
     Assert.assertEquals("2015-05-03 14:29", formattedDate);
   }
 
