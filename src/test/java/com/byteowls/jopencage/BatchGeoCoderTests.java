52ecc2d17a9937e71fe768281c6d8cfad20f7d48
Michael Oberwasserlechner <michael@byteowls.com>
2019-02-22 13:55:57 +0100

    refactor(): #24: replace system.out with logger and move reading files to own method

--- src/test/java/com/byteowls/jopencage/BatchGeoCoderTests.java
+++ src/test/java/com/byteowls/jopencage/BatchGeoCoderTests.java
@@ -9,17 +9,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.byteowls.jopencage.model.JOpenCageForwardRequest;
 import com.byteowls.jopencage.model.JOpenCageLatLng;
 import com.byteowls.jopencage.model.JOpenCageResponse;
 import com.byteowls.jopencage.model.JOpenCageReverseRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class BatchGeoCoderTests {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(BatchGeoCoderTests.class);
+
+  private static final String REVERSE_SAMPLES = "batchGeoCoderInput/reverseBatchSample.txt";
+  private static final String FORWARD_SAMPLES = "batchGeoCoderInput/forwardBatchSample.txt";
 
   private JOpenCageGeocoder jOpenCageGeocoder;
 
@@ -31,77 +38,68 @@ public class BatchGeoCoderTests {
     }
     this.jOpenCageGeocoder = new JOpenCageGeocoder(apiKey);
   }
-  
-  @Test
-  @Ignore
-  public void forwardBatchTest() {
-    List<String> listOfAddresses = new ArrayList<>();
-        
-    InputStream inputFile = getClass().getClassLoader().getResourceAsStream("batchGeoCoderInput/forwardBatchSample.txt"); 
-    
-    try (BufferedReader br = new BufferedReader(new InputStreamReader(inputFile, StandardCharsets.UTF_8))) {
-      listOfAddresses = br.lines().collect(Collectors.toList());
-    } catch (IOException e) {
-      System.out.println("Unable to read file");
-      e.printStackTrace();
-    }
-  
-    System.out.println("Latitude, Longitude:");
-    listOfAddresses.forEach(address -> {
-      try {
-        JOpenCageForwardRequest request = new JOpenCageForwardRequest(address);
-        request.setLimit(1);
-        request.setNoAnnotations(true);
-        JOpenCageResponse response = this.jOpenCageGeocoder.forward(request);
-        Thread.sleep(1000); // free trial accounts are limited to 1 request/second
-        if (response != null) {
-          JOpenCageLatLng coordinates = response.getResults().get(0).getGeometry();
-          System.out.println(coordinates.getLat().toString() + "," + coordinates.getLng().toString());
-        } else {
-          System.out.println("Unable to geocode input address: " + address);
-        }
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }    
-    });
 
+  private List<String> getSamplesFromFile(String path) {
+    List<String> list = new ArrayList<>();
+
+    InputStream inputFile = getClass().getClassLoader().getResourceAsStream(path);
+    Assert.assertNotNull(inputFile);
+
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(inputFile, StandardCharsets.UTF_8))) {
+      list = br.lines().collect(Collectors.toList());
+    } catch (IOException e) {
+      LOGGER.error("Unable to read file '" + path + "'", e);
+    }
+    return list;
   }
-  
-  @Test
-//  @Ignore
-  public void reverseBatchTest() {
-    List<String> listOfCoordinatePairs = new ArrayList<>();
-    
-    InputStream inputFile = getClass().getClassLoader().getResourceAsStream("batchGeoCoderInput/reverseBatchSample.txt"); 
-    
-    try (BufferedReader br = new BufferedReader(new InputStreamReader(inputFile, StandardCharsets.UTF_8))) {
-      listOfCoordinatePairs = br.lines().collect(Collectors.toList());
-    } catch (IOException e) {
-      System.out.println("Unable to read file");
-      e.printStackTrace();
-    }
-   
-    System.out.println("Formatted addresses:");
-    listOfCoordinatePairs.forEach(coordinatePair -> {
-      try {
-        Double lat = Double.valueOf(coordinatePair.split(",")[0]);
-        Double lng = Double.valueOf(coordinatePair.split(",")[1]);
-        JOpenCageReverseRequest request = new JOpenCageReverseRequest(lat, lng);
-        request.setLimit(1);
-        request.setNoAnnotations(true);
-        JOpenCageResponse response = jOpenCageGeocoder.reverse(request);
-        Thread.sleep(1000); // free trial accounts are limited to 1 request/second
-        if (response != null) {
-          String formattedAddress = response.getResults().get(0).getFormatted().toString();
-          System.out.println(formattedAddress);
-        } else {
-          System.out.println("Unable to find address for input coordinates: " + coordinatePair);
-        }
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    });
 
+  @Test
+  public void forwardBatchTest() {
+    List<String> addresses = getSamplesFromFile(FORWARD_SAMPLES);
+    for (String address : addresses) {
+      JOpenCageForwardRequest request = new JOpenCageForwardRequest(address);
+      request.setLimit(1);
+      request.setNoAnnotations(true);
+
+      JOpenCageResponse response = this.jOpenCageGeocoder.forward(request);
+      Assert.assertTrue(response != null && response.getResults() != null && !response.getResults().isEmpty());
+
+      JOpenCageLatLng coordinates = response.getResults().get(0).getGeometry();
+
+      LOGGER.info("Coordinate: {},{}", coordinates.getLat(), coordinates.getLng());
+      Assert.assertNotNull(coordinates);
+
+      try {
+        Thread.sleep(1000); // free trial accounts are limited to 1 request/second
+      } catch (InterruptedException e) {
+        LOGGER.error("", e);
+      }
+    }
+  }
+
+  @Test
+  public void reverseBatchTest() {
+    for (String coordinatePair : getSamplesFromFile(REVERSE_SAMPLES)) {
+      Double lat = Double.valueOf(coordinatePair.split(",")[0]);
+      Double lng = Double.valueOf(coordinatePair.split(",")[1]);
+      JOpenCageReverseRequest request = new JOpenCageReverseRequest(lat, lng);
+      request.setLimit(1);
+      request.setNoAnnotations(true);
+      JOpenCageResponse response = jOpenCageGeocoder.reverse(request);
+
+      Assert.assertTrue("Unable to find address for coordinates: " + coordinatePair, response != null && response.getResults() != null && !response.getResults().isEmpty());
+
+      String formattedAddress = response.getResults().get(0).getFormatted();
+
+      LOGGER.info("Adress: {}", formattedAddress);
+      Assert.assertNotNull(formattedAddress);
+
+      try {
+        Thread.sleep(1000); // free trial accounts are limited to 1 request/second
+      } catch (InterruptedException e) {
+        LOGGER.error("", e);
+      }
+    }
   }
 
 }
