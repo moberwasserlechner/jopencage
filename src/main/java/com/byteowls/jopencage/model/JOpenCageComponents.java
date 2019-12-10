f63100ebc3fcb4ac99aed022b9dcb78e98b77528
Michael Oberwasserlechner <michael.oberwasserlechner@senacor.com>
2019-12-10 23:04:42 +0100

    feat(): add components fields

--- src/main/java/com/byteowls/jopencage/model/JOpenCageComponents.java
+++ src/main/java/com/byteowls/jopencage/model/JOpenCageComponents.java
@@ -9,7 +9,10 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class JOpenCageComponents {
   @JsonProperty("ISO_3166-1_alpha-2")
-  private String iso31661;
+  private String iso31661Alpha2;
+  @JsonProperty("ISO_3166-1_alpha-3")
+  private String iso31661Alpha3;
+
   @JsonProperty("_type")
   private String type;
   private String city;
@@ -19,18 +22,39 @@ public class JOpenCageComponents {
   @JsonProperty("country_code")
   private String countryCode;
   private String county;
+  private String pedestrian;
   private String postcode;
   private String road;
+  @JsonProperty("road_type")
+  private String roadType;
   @JsonProperty("house_number")
   private String houseNumber;
+  private String neighbourhood;
   private String state;
+  @JsonProperty("state_code")
+  private String stateCode;
+  @JsonProperty("state_district")
+  private String stateDistrict;
+  private String region;
   @JsonProperty("political_union")
   private String politicalUnion;
   private String suburb;
   private String town;
+  private String village;
 
+  /**
+   * @deprecated use {@link #getIso31661Alpha2()} instead
+   */
   public String getISO31661() {
-    return iso31661;
+    return iso31661Alpha2;
+  }
+
+  public String getIso31661Alpha2() {
+    return iso31661Alpha2;
+  }
+
+  public String getIso31661Alpha3() {
+    return iso31661Alpha3;
   }
 
   public String getType() {
@@ -57,6 +81,10 @@ public class JOpenCageComponents {
     return county;
   }
 
+  public String getPedestrian() {
+    return pedestrian;
+  }
+
   public String getPostcode() {
     return postcode;
   }
@@ -65,12 +93,32 @@ public class JOpenCageComponents {
     return road;
   }
 
+  public String getRoadType() {
+    return roadType;
+  }
+
   public String getHouseNumber() {
     return houseNumber;
   }
 
+  public String getNeighbourhood() {
+    return neighbourhood;
+  }
+
   public String getState() {
     return state;
+  }
+
+  public String getStateCode() {
+    return stateCode;
+  }
+
+  public String getStateDistrict() {
+    return stateDistrict;
+  }
+
+  public String getRegion() {
+    return region;
   }
 
   public String getPoliticalUnion() {
@@ -83,5 +131,9 @@ public class JOpenCageComponents {
 
   public String getTown() {
     return town;
+  }
+
+  public String getVillage() {
+    return village;
   }
 }
